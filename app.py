ee968a5a203f4b81cd4eb79777cdb3eb7a1221f2
jompoj <jompoj.bsjtp@gmail.com>
2020-09-21 13:25:34 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,188 @@
+from flask import Flask, render_template, url_for, request, redirect
+import matplotlib.pyplot as plt
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import glob
+import os
+
+
+#import riptide
+#from candidate.py import Candidate
+global FFA_img_path
+FFA_img_path='/static/images/HTRUS/' #path to images(.png) from the ffa 
+global FFA_list_file
+FFA_list_file=[]
+
+global presto_img_path
+presto_img_path='/home/jompoj/HTRU-S_results' #path to images(.png) from the presto ((To be implemented in the future))
+global presto_list_file
+presto_list_file=[]
+
+global presto_pfd_img_path
+presto_pfd_img_path='/home/jompoj/HTRU-S_results' #path to archives(.pfd) from the presto ((To be implemented in the future with sgan and pics))
+global presto_pfd_list_file
+presto_pfd_file=[]
+global current_id #Global dummy id for id 
+current_id = 0
+
+global current_row #Global dummy id for row
+current_row = 0
+
+
+
+
+app = Flask(__name__)
+#app.config['UPLOAD_FOLDER'] = FFA_img_path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///htrusll.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    score = db.Column(db.Integer, nullable=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return self.id
+
+
+#global rowtoid
+
+
+def rowtoid(row):
+    row_=compare_rowtoid()
+    return row_[row]
+
+def compare_rowtoid():
+    row_list=[]
+    for i in Todo.query.all():
+        row_list.append(i.id)
+    return row_list
+
+def get_ffa_images():
+    global FFA_list_file #Call global parameter FFA_list_file 
+    global FFA_img_path #Call the path
+    FFA_list_file=sorted(glob.glob('/home/jompoj/flask_PFINDER'+FFA_img_path+'/*.png'))
+    #rint(FFA_list_file)
+
+@app.route('/')
+def plot():
+    global FFA_list_file
+    global current_row
+    global current_id
+    #Todo.query.delete()
+    load_files_to_db()
+    if len(FFA_list_file)==0:
+        return render_template('index.html', url=FFA_list_file[0], url2='/static/images/C1_2020.pfd.png', sgan_score='1', pics_score='1', tasks=['None','None','none'], id='none', name='Dummy')
+    elif current_row<=len(Todo.query.all()):
+        current_id=rowtoid(current_row)
+        tasks = Todo.query.order_by(Todo.id).all()
+        #return render_template('plot.html', url=FFA_list_file[0])
+        return render_template('index.html', url='/static/images/HTRUS/'+FFA_list_file[current_row].split('/')[-1] , url2='/static/images/C1_2020.pfd.png', sgan_score='1', pics_score='1', tasks=tasks, id=current_id)
+
+
+@app.route('/')
+def get_sgan_score():
+    return 1
+
+#@app.route('/', methods=['POST', 'GET'])
+#def uploadfile(): 
+#    if request.method == 'POST':
+#        task_content = request.form['content']
+#        new_task = Todo(content=task_content)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def submit(): 
+    global current_row
+    global current_id
+    global rowtoid
+    #return request.form
+    if request.method == "POST":
+        if request.form.get("RFI"):
+            score=0
+        elif request.form.get("Known"):
+            score=5
+        elif request.form.get("HarmKnown"):
+            score=4    
+        elif request.form.get("ClassA"):
+            score=3
+        elif request.form.get("ClassB"):
+            score=2
+        elif request.form.get("back"):
+            score=-999 #Using -999 to tell the function update() not to update
+        elif request.form.get("next"):
+            score=999 #Using 999 to tell the function update() not to update
+        elif request.form.get("jump_to"):
+            score=-1000
+            id_to=request.form["content"]
+            current_id=int(id_to)
+            #return str(id_to)
+        elif request.form.get("restart"):
+            Todo.query.delete()
+            return redirect('/')
+    update(score)
+    return redirect('/')
+
+def back():
+    global current_row
+    global current_id
+    global rowtoid
+    current_id=rowtoid(current_row-1)
+    current_row=current_row-1
+
+def next():
+    global current_row
+    global current_id
+    global rowtoid
+    current_id=rowtoid(current_row+1)
+    current_row=current_row+1
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        compare_rowtoid()
+        return redirect('/')
+        
+    except:
+        return 'There was a problem deleting that task'
+
+def update(score):
+    global current_id
+    global rowtoid
+    global current_row
+    if score == 999:
+        next()
+    elif score == -999:
+        back()
+    elif score < 10:
+        task = Todo.query.get(current_id)
+        task.score = score
+        db.session.commit()
+        next()
+
+        
+
+def load_files_to_db():
+    get_ffa_images()
+    global FFA_list_file
+    try:
+        for i in FFA_list_file:
+            print(i)
+            if bool(Todo.query.filter_by(name=i).first()):
+                print("The file is here")
+            else:
+                new_file = Todo(name=i)
+                db.session.add(new_file)
+                db.session.commit()
+                print("Add the file to the database")
+        #return redirect('/')
+    except IndexError:
+        return "No file in this project folder"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=1222)
---
+++ libffa.py
@@ -0,0 +1,229 @@
+### Standard library imports
+import os
+import ctypes
+
+### Non-standard imports
+import numpy as np
+import numpy.ctypeslib as npct
+from numpy import log, sin, cos, exp, pi
+
+### Local imports
+from .ffautils import generate_width_trials
+
+###############################################################################
+
+def load_libffa():
+    # this file's dir/name
+    fdir, fname = os.path.split(os.path.abspath(__file__))
+
+    # load the library, using numpy mechanisms
+    lib = npct.load_library(
+        os.path.join(fdir, 'c_src', 'libffa'), '.'
+        )
+
+    ### Set argument and return types of the library's functions
+    lib.py_transform.restype = None
+    lib.py_transform.argtypes = [
+        npct.ndpointer(dtype=np.float32, ndim=2, flags=('C_CONTIGUOUS', 'ALIGNED')),
+        ctypes.c_size_t,
+        ctypes.c_size_t,
+        npct.ndpointer(dtype=np.float32, ndim=2, flags=('C_CONTIGUOUS', 'ALIGNED', 'WRITEABLE')),
+        ]
+
+    lib.get_snr_2d.restype = None
+    lib.get_snr_2d.argtypes = [
+        npct.ndpointer(dtype=np.float32, ndim=2, flags=('C_CONTIGUOUS', 'ALIGNED')),
+        ctypes.c_size_t,
+        ctypes.c_size_t,
+        npct.ndpointer(dtype=np.int64, ndim=1, flags=('C_CONTIGUOUS', 'ALIGNED')),
+        ctypes.c_size_t,
+        ctypes.c_double,
+        npct.ndpointer(dtype=np.float32, ndim=2, flags=('C_CONTIGUOUS', 'ALIGNED', 'WRITEABLE')),
+        ]
+
+    lib.downsample.restype = None
+    lib.downsample.argtypes = [
+        npct.ndpointer(dtype=np.float32, ndim=1, flags=('C_CONTIGUOUS', 'ALIGNED')), # input
+        ctypes.c_size_t, # input size
+        ctypes.c_double, # downsampling factor
+        npct.ndpointer(dtype=np.float32, ndim=1, flags=('C_CONTIGUOUS', 'ALIGNED', 'WRITEABLE')) # output
+        ]
+
+    lib.py_periodogram.restype = None
+    lib.py_periodogram.argtypes = [
+        npct.ndpointer(dtype=np.float32, ndim=1, flags=('C_CONTIGUOUS', 'ALIGNED')), # input
+        ctypes.c_size_t, # input size
+        npct.ndpointer(dtype=float, ndim=1, flags=('C_CONTIGUOUS', 'ALIGNED')), # sequence: dsfactor
+        npct.ndpointer(dtype=int, ndim=1, flags=('C_CONTIGUOUS', 'ALIGNED')), # sequence: bins_min
+        npct.ndpointer(dtype=int, ndim=1, flags=('C_CONTIGUOUS', 'ALIGNED')), # sequence: bins_max
+        ctypes.c_size_t, # number of ProcessingPlan steps
+        npct.ndpointer(dtype=int, ndim=1, flags=('C_CONTIGUOUS', 'ALIGNED')), # sequence: widths
+        ctypes.c_size_t, # number of width trials
+        npct.ndpointer(dtype=np.float32, ndim=1, flags=('C_CONTIGUOUS', 'ALIGNED', 'WRITEABLE')), # period trials (output)
+        npct.ndpointer(dtype=np.float32, ndim=1, flags=('C_CONTIGUOUS', 'ALIGNED', 'WRITEABLE')), # output S/N
+        ]
+    return lib
+
+
+class LibFFA(object):
+    """ A class that wraps all C functions as static class members """
+    _lib = load_libffa()
+    py_transform = _lib.py_transform
+    get_snr_2d = _lib.get_snr_2d
+    downsample = _lib.downsample
+    py_periodogram = _lib.py_periodogram
+
+###############################################################################
+
+def generate_signal(nsamp, period, phi0=0.0, ducy=0.01, amplitude=1.0, stdnoise=1.0):
+    """ Generate a time series containing a periodic signal with a von Mises
+    pulse profile. This function is mostly for test purposes.
+
+    Parameters
+    ----------
+    nsamp: int
+        Number of samples to generate.
+    period: float
+        Period in number of samples.
+    phi0: float
+        Initial pulse phase in number of periods.
+    ducy: float
+        Duty cycle of the pulse.
+    amplitude: float
+        Pulse amplitude.
+    stdnoise: float
+        Standard deviation of the background noise.
+
+    Returns
+    -------
+    tseries: ndarray (1D, float)
+        Output time series.
+    """
+    # von mises parameter
+    kappa = log(2.0) / (2.0 * sin(pi*ducy/2.0)**2)
+
+    # Generate pulse train
+    phase_radians = (np.arange(nsamp, dtype=float) / period - phi0) * (2 * pi)
+    signal = amplitude * exp(kappa*(cos(phase_radians) - 1.0))
+
+    # Add noise
+    if stdnoise > 0.0:
+        noise = np.random.normal(size=nsamp, loc=0.0, scale=stdnoise)
+    else:
+        noise = 0.0
+
+    tseries = signal + noise
+    return tseries
+
+
+def ffa_transform_2d(data):
+    """ Compute the FFA transform of an array of profiles.
+
+    Parameters
+    ----------
+    data: ndarray (2D)
+        Input time series data in two-dimensional shape (m, b), where m
+        is the number of signal periods and b the number of phase bins.
+
+    Returns
+    -------
+    transform: ndarray (2D)
+        The FFA transform of 'data', as a 2D array of shape (m, b).
+        Line number k corresponds to a summation path with a top-to-bottom
+        right shift of k bins.
+    """
+    m, b = data.shape
+    output = np.zeros(shape=(m,b), dtype=np.float32)
+    LibFFA.py_transform(data.astype(np.float32), m, b, output)
+    return output
+
+
+def ffa_transform_1d(data, pnum):
+    """ Compute the FFA transform of a time series at a given integer
+    period number. The algorithm only considers a number of time
+    samples that is a multiple of 'pnum', and therefore ignores the
+    last nsamp % pnum samples.
+
+    Parameters
+    ----------
+    data: ndarray (1D)
+        Input time series data. Trailing samples with indices larger
+        than the largest multiple of pnum are ignored.
+    pnum: int
+        Signal period in samples.
+
+    Returns
+    -------
+    transform: ndarray (2D)
+        The FFA transform of 'data', as a 2D array of shape
+        (m, pnum), where m is the number of complete periods of the signal.
+        Line number k corresponds to a summation path with a top-to-bottom
+        right shift of k bins.
+    """
+    m = data.size // pnum
+    return ffa_transform_2d(data[:m*pnum].reshape(m, pnum))
+
+
+def get_snr(data, stdnoise=1.0):
+    """ Compute the S/N ratio of pulse profile(s) for a range of boxcar width
+    trials.
+
+    Parameters
+    ----------
+    data: ndarray
+        Input profile(s). Can be of any shape, as long as the last axis
+        is pulse phase.
+    stdnoise: float
+        Standard deviation of the background noise in all profiles.
+
+    Returns
+    -------
+    snr: ndarray
+        Output with the same shape as data, except for the last axis
+        which represents trial pulse width index.
+    widths: ndarray, 1D
+        Trial pulse widths, an array that has the same length as the last
+        axis of 'snr'.
+    """
+    # Number of bins is the length of the last axis
+    b = data.shape[-1]
+
+    # Input to C function must be 2D
+    cinput = data.reshape(-1, b).astype(np.float32)
+    m = cinput.shape[0]
+
+    # Width trials
+    widths = generate_width_trials(b, ducy_max=0.5, wtsp=1.5)
+    nw = widths.size
+
+    # Prepare output
+    out = np.zeros(shape=(m, nw), dtype=np.float32)
+    LibFFA.get_snr_2d(cinput, m, b, widths, nw, stdnoise**2.0, out)
+
+    # Reshape output properly
+    shape = list(data.shape[:-1]) + [nw]
+    out = out.reshape(shape)
+    return out, widths
+
+
+def downsample(data, factor):
+    """ Downsample an array by a real-valued factor.
+
+    Parameters
+    ----------
+    data: array_like
+        Time series data to downsample.
+    factor: float
+        Downsampling factor.
+
+    Returns
+    -------
+    out: ndarray, float32
+        Downsampled data.
+    """
+    if not factor > 1:
+        raise ValueError('factor must be > 1')
+    outsize = int(len(data) / float(factor))
+    out = np.zeros(outsize, dtype=np.float32)
+    LibFFA.downsample(np.asarray(data, dtype=np.float32), len(data), factor, out)
+    return out
