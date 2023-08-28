74af73390f09c82e921e95000dbb8b3b66d4cce1
JMJ_WONG <19991642+Boltfox@users.noreply.github.com>
2023-08-28 17:17:09 +0200

    now we can change the project name

--- app.py
+++ app.py
@@ -4,9 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import glob
 import os
+import shutil
+from sqlalchemy import create_engine
+import json
+
+global files_loaded
+files_loaded = False
+
+with open("config.json", "r") as f:
+    config_dict = json.load(f)
+
+
+pj = config_dict['current_project']
 
 sd = os.path.dirname(os.path.abspath(__file__))
-pj = 'HTRUS'
+#pj = 'HTRUS'
 
 #import riptide
 #from candidate.py import Candidate
@@ -35,8 +47,13 @@ current_row = 0
 
 app = Flask(__name__)
 #app.config['UPLOAD_FOLDER'] = FFA_img_path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///htrusll.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = config_dict['database_uri']
+
+
+def create_db(app):
+    db = SQLAlchemy(app)
+    return db
+db = create_db(app)
 
 
 
@@ -51,6 +68,40 @@ class Todo(db.Model):
 
 #db.create_all()
 #global rowtoid
+@app.route('/config', methods=['GET', 'POST'])
+def config():
+    if request.method == 'POST':
+        new_project_name = request.form['new_project_name']
+        update_project_name(new_project_name)
+        return redirect('/')
+    return render_template('config.html')
+
+
+def update_project_name(new_project_name):
+    global config_dict  # Declare config_dict as global if it's defined outside this function
+    
+    # Update the configuration dictionary
+    config_dict['current_project'] = new_project_name
+    config_dict['database_uri'] = f'sqlite:///{new_project_name}.db'
+    
+    # Save the updated configuration back to the JSON file
+    with open("config.json", "w") as f:
+        json.dump(config_dict, f)
+    
+    print("Configuration updated. Please restart the application.")
+
+
+def rename_folder_and_db(old_project_name, new_project_name):
+    # Rename image folder
+    old_folder_path = os.path.join(sd, "static", "images", old_project_name)
+    new_folder_path = os.path.join(sd, "static", "images", new_project_name)
+    shutil.move(old_folder_path, new_folder_path)
+    
+    # Rename database
+    old_db_path = f"{old_project_name}.db"
+    new_db_path = f"{new_project_name}.db"
+    os.rename(old_db_path, new_db_path)
+
 
 
 def rowtoid(row):
@@ -79,8 +130,11 @@ def get_ffa_images():
 def plot():
     global FFA_list_file
     global current_row
-    global current_id
-    load_files_to_db()
+    global current_id 
+    global files_loaded
+
+    if not files_loaded:
+        load_files_to_db()
     # Check if all images have been labeled
     if current_row >= len(FFA_list_file):
         current_row = 0  # Reset to the first image
@@ -93,7 +147,7 @@ def plot():
     elif current_row < len(Todo.query.all()):
         current_id = rowtoid(current_row)
         tasks = Todo.query.order_by(Todo.id).all()
-        return render_template('index.html', url='/static/images/HTRUS/' + FFA_list_file[current_row].split('/')[-1], url2='/static/images/C1_2020.pfd.png', sgan_score='1', pics_score='1', tasks=tasks, id=current_id)
+        return render_template('index.html', url='/static/images/'+pj+'/'+ FFA_list_file[current_row].split('/')[-1], url2='/static/images/C1_2020.pfd.png', sgan_score='1', pics_score='1', tasks=tasks, id=current_id)
 
 
 @app.route('/')
@@ -147,6 +201,10 @@ def submit():
             Todo.query.delete()
             db.session.commit()
             return redirect('/')
+        elif request.form.get("change_project"):
+            new_project_name = request.form["new_project_name"]
+            update_project_name(new_project_name)
+            return redirect('/')
     update(score)
     return redirect('/')
 
@@ -194,6 +252,10 @@ def update(score):
         
 
 def load_files_to_db():
+    global files_loaded
+    if files_loaded:
+        return  # Skip if files are already loaded
+    
     get_ffa_images()
     global FFA_list_file
     try:
@@ -206,6 +268,8 @@ def load_files_to_db():
                 db.session.add(new_file)
                 db.session.commit()
                 print("Add the file to the database")
+        files_loaded = True  # Set flag to True after first load
+
         #return redirect('/')
     except IndexError:
         return "No file in this project folder ok?"
